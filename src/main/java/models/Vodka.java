17563e5b22d822e4b7800fa436344f6686db8f62
moffd234 <moffd234@gmail.com>
2024-04-06 16:26:20 -0400

    Added Vodka properties

--- src/main/java/models/Vodka.java
+++ src/main/java/models/Vodka.java
@@ -1,4 +1,24 @@
 package models;
 
 public class Vodka {
+    private String brand;
+    private int proof;
+    private BottleSize bottleSize;
+}
+
+enum BottleSize{
+    pint(.375),
+    fifth(.750),
+    liter(1),
+    handle(1.75);
+
+    private final double liters;
+
+    BottleSize(double liters) {
+        this.liters = liters;
+    }
+
+    public double getLiters() {
+        return liters;
+    }
 }
