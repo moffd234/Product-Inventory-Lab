b1c33391515e1f4b04c8a02928c7efb90148394b
moffd234 <moffd234@gmail.com>
2024-04-06 16:06:12 -0400

    Added kit properties

--- src/main/java/models/Kits.java
+++ src/main/java/models/Kits.java
@@ -1,4 +1,19 @@
 package models;
 
 public class Kits {
+    private int id;
+    private int kitNum;
+    private int quantity;
+    private double price;
+    private String team;
+    private String color;
+    private String brand;
+    private String player;
+    private Size size;
+
+}
+enum Size{
+    SMALL,
+    MEDIUM,
+    LARGE;
 }
