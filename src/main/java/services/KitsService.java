0d743705014c5317e34ba1e064f3e95b7d33a238
moffd234 <moffd234@gmail.com>
2024-04-08 14:09:04 -0400

    Created test for KitService and implemented code to pass the test

--- src/main/java/services/KitsService.java
+++ src/main/java/services/KitsService.java
@@ -1,4 +1,18 @@
 package services;
 
+import models.Kit;
+import models.KitSize;
+
+import java.util.ArrayList;
+
 public class KitsService {
+    private ArrayList<Kit> kitsInventory = new ArrayList<>();
+    private static int nextId = 1;
+
+    public Kit create(int kitNum, int quantity, double price, String team,
+                      String brand, String player, KitSize kitSize){
+        Kit createdKit = new Kit(++nextId, kitNum, quantity, price, team, brand, player, kitSize);
+        kitsInventory.add(createdKit);
+        return createdKit;
+    }
 }
--- src/test/java/services/KitsServiceTest.java
+++ src/test/java/services/KitsServiceTest.java
@@ -1,4 +1,48 @@
 package services;
 
+import models.Kit;
+import models.KitSize;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class KitsServiceTest {
+    @Test
+    public void createTest(){
+
+        // (1)
+        String expectedPlayer = "Messi";
+        String expectedBrand = "Adidas";
+        String expectedTeam = "Barcelona";
+        KitSize expectedSize = KitSize.MEDIUM;
+        int expectedKitNum = 10;
+        int expectedQty = 10;
+        double expectedPrice = 80.00f;
+
+        // (2)
+        KitsService kitsService = new KitsService();
+        Kit testKit = kitsService.create(expectedKitNum, expectedQty, expectedPrice,
+                expectedTeam, expectedBrand, expectedPlayer, expectedSize);
+
+        // (3)
+        int actualId = testKit.getId();
+        String actualPlayer = testKit.getPlayer();
+        String actualBrand = testKit.getBrand();
+        String actualTeam = testKit.getTeam();
+        int actualKitNum = testKit.getKitNum();
+        int actualQty = testKit.getQuantity();
+        double actualPrice = testKit.getPrice();
+        KitSize actualSize = testKit.getSize();
+
+        // (4)
+        Assert.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assert.assertEquals((Double) expectedPrice, (Double) actualPrice);
+        Assert.assertEquals(expectedBrand, actualBrand);
+        Assert.assertEquals(expectedPlayer, actualPlayer);
+        Assert.assertEquals(expectedSize, actualSize);
+        Assert.assertEquals(expectedQty, actualQty);
+        Assert.assertEquals(expectedTeam, actualTeam);
+        Assert.assertEquals(expectedKitNum, actualKitNum);
+
+    }
+
 }
